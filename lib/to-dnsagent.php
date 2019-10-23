468f56003f0b6f7c50c216a34939e5ffdce8af32
sempre harapeco <scin.ichi@gmail.com>
2019-10-23 10:46:49 +0900

    DNSAgent: combine rules per five hosts

--- lib/to-dnsagent.php
+++ lib/to-dnsagent.php
@@ -2,24 +2,27 @@
 const SRC = __DIR__ . '/../src/blocklist.txt';
 const DEST = __DIR__ . '/../out/rules.cfg';
 
+const HOSTS_PER_RULE = 5;
+
 $lines = [];
 
 $hosts = @file(SRC, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
 sort($hosts);
 
+$escapeRE = function($str) {
+	$str = preg_replace('/([\\.])/', '\\\\$0', $str);
+	$str = preg_replace('/\\*/', '.*', $str);
+	return $str;
+};
+
 $config = [];
-foreach ($hosts as $host) {
-	$host = trim($host, '*');
+for ($i = 0, $len = count($hosts); $i < $len; $i += HOSTS_PER_RULE) {
+	$lHosts = array_slice($hosts, $i, HOSTS_PER_RULE);
+	$pattern = implode('|', array_map($escapeRE, $lHosts));
 	$config[] = [
-		'Pattern' => escapeRE($host) . '$',
+		'Pattern' => "($pattern)$",
 		'Address' => '0.0.0.0',
 	];
 }
 
 file_put_contents(DEST, json_encode($config, JSON_PRETTY_PRINT));
-
-function escapeRE($str) {
-	$str = preg_replace('/([\\.])/', '\\\\$0', $str);
-	$str = preg_replace('/\\*/', '.*', $str);
-	return $str;
-}
